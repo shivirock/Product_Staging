5c62d42950d2d4c8107d12c085072b285ee9c137
shivi <shivirock94@gmail.com>
2020-09-28 21:38:58 +0530

    Added method to Fetch Details based on parameters match

--- src/main/java/com/karmaya/fulvila/repository/AdminRepository.java
+++ src/main/java/com/karmaya/fulvila/repository/AdminRepository.java
@@ -4,4 +4,6 @@ import com.karmaya.fulvila.model.Admin;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface AdminRepository extends MongoRepository<Admin, Long> {
+    Admin findByEmail(String email);
+    Admin findByEmailAndAuthProvider(String email, Admin.AuthProvider authProvider);
 }
